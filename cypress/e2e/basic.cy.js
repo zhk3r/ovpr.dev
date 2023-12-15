4579f90db9bfe0ab6b413a179e875906cdf4c6a7
zhk3r <nobody@haz.space>
2023-12-15 18:35:15 +0100

    cy.js fix?

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -1,19 +1,20 @@
-describe('ovpr.dev pt', () => {
+describe('ovpr.dev', () => {
   beforeEach(() => {
     cy.visit('/') // Visit the home page before each test
   })
 
   it('displays welcome text', () => {
     cy.get('h1') // Select the h1 element
-      .contains('ovpr.dev'); // Check if it contains the welcome text
+      .contains('just a simple little site'); // Check if it contains the welcome text
   })
 
-  it('has functional repository links', () => {
+  it('has correct repository links', () => {
+    // Test if the repository links are as expected
     cy.get('a.repo-button') // Select all repository buttons
-      .each(($el) => {
-        cy.wrap($el) // Wrap each button for individual testing
-          .should('have.attr', 'href') // Check if it has an href attribute
-          .and('include', 'github.com/ovpr-dev'); // Ensure the href includes the correct URL
-      })
+      .should('have.attr', 'href') // Check if it has an href attribute
+      .then((hrefs) => {
+        expect(hrefs).to.include('https://github.com/ovpr-dev/repo1'); // Ensure the first href includes the correct URL
+        expect(hrefs).to.include('https://github.com/ovpr-dev/repo2'); // Ensure the second href includes the correct URL
+      });
   })
 })
