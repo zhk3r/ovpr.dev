bd6e0010db89694d860ef2e798dd1b7a00c8acea
zhk3r <nobody@haz.space>
2023-12-15 18:39:19 +0100

    Now it might be fixed.

--- cypress/e2e/ovpr.dev.cy.js
+++ cypress/e2e/ovpr.dev.cy.js
@@ -5,7 +5,7 @@ describe('ovpr.dev', () => {
 
   it('displays welcome text', () => {
     cy.get('h1') // Select the h1 element
-      .contains('just a simple little site'); // Check if it contains the welcome text
+      .contains('ovpr.dev'); // Check if it contains the welcome text
   })
 
   it('has correct repository links', () => {
@@ -13,8 +13,8 @@ describe('ovpr.dev', () => {
     cy.get('a.repo-button') // Select all repository buttons
       .should('have.attr', 'href') // Check if it has an href attribute
       .then((hrefs) => {
-        expect(hrefs).to.include('https://github.com/ovpr-dev/repo1'); // Ensure the first href includes the correct URL
-        expect(hrefs).to.include('https://github.com/ovpr-dev/repo2'); // Ensure the second href includes the correct URL
+        expect(hrefs).to.include('https://github.com/zhk3r/check'); // Ensure the first href includes the correct URL
+        expect(hrefs).to.include('https://github.com/zhk3r/ovpr.dev'); // Ensure the second href includes the correct URL
       });
   })
 })
