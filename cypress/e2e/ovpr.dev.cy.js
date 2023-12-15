406bd80176cd6950b947dca432055011b7211a11
zhk3r <nobody@haz.space>
2023-12-15 18:42:21 +0100

    Mkay. Again.

--- cypress/e2e/ovpr.dev.cy.js
+++ cypress/e2e/ovpr.dev.cy.js
@@ -13,8 +13,8 @@ describe('ovpr.dev', () => {
     cy.get('a.repo-button') // Select all repository buttons
       .should('have.attr', 'href') // Check if it has an href attribute
       .then((hrefs) => {
-        expect(hrefs).to.include('https://github.com/zhk3r/check'); // Ensure the first href includes the correct URL
-        expect(hrefs).to.include('https://github.com/zhk3r/ovpr.dev'); // Ensure the second href includes the correct URL
+        expect(hrefs).to.include('https://github.com/zhk3r/ovpr.dev'); // Ensure the first href includes the correct URL
+        expect(hrefs).to.include('https://github.com/zhk3r/check'); // Ensure the second href includes the correct URL
       });
   })
 })
